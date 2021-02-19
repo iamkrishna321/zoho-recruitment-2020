2d9f20d3fb1000a8a93318844dcd77b1bcc917ba
Krishnakanth <60458521+iamkrishna321@users.noreply.github.com>
2021-02-19 09:08:00 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class Test {
+
+
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		//scanner.nextLine();
+		
+		while(scanner.hasNextInt()) {
+			System.out.println(scanner.nextInt());
+		}
+		
+		
+		
+	}
+		
+}
